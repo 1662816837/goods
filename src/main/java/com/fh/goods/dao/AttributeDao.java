a43ad3a10c02d3fd206535a5b03e3f22eed0fd74
yxr <1662816837@qq.com>
2021-01-16 21:44:07 +0800

    修改了基于逻辑删除的查询

--- src/main/java/com/fh/goods/dao/AttributeDao.java
+++ src/main/java/com/fh/goods/dao/AttributeDao.java
@@ -12,11 +12,12 @@ import java.util.List;
 public interface AttributeDao {
     @Select("<script> select count(1) from goods_attribute where 1=1 " +
             "<if test='name != null and name != &quot;&quot;'> and name like CONCAT('%',#{name},'%')</if>" +
-            "</script>")
+            " and isDel = 0 </script>")
     public Long queryCount(PageParam param);
     @Select("<script> select * from goods_attribute where 1=1 " +
             "<if test='name != null and name != &quot;&quot;'> and name like CONCAT('%',#{name},'%')</if>" +
-            "limit #{startIndex},#{limit}  </script>")
+            " and isDel = 0 " +
+            "limit #{startIndex},#{limit}    </script>")
     public List<Attribute> queryData(PageParam param);
     @Options(useGeneratedKeys=true, keyProperty="id", keyColumn="id")
     @Insert("insert into goods_attribute (name,nameCH,typeId,type,isSKU,isDel,createDate,author) values (#{name},#{nameCH},#{typeId},#{type},#{isSKU},#{isDel},#{createDate},#{author})")
